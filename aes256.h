7444df8b5b37b0d2489230adb3a056b77eb5d05f
Alex Lambley <lambleyalexander@gmail.com>
2018-10-18 17:40:34 -0400

    init

---
+++ aes256.h
@@ -0,0 +1,29 @@
+#ifndef AES256_H
+#define AES256_H
+
+#include <stdint.h>
+#include <math.h>
+
+typedef enum { false, true } bool;
+typedef struct AES256_Word { uint8_t x[4]; } AES256_Word;
+typedef struct AES256_State { AES256_Word x[4]; } AES256_State;
+
+AES256_State AES256Encrypt(AES256_State, uint8_t*);
+AES256_State AES256Decrypt(AES256_State, uint8_t*);
+void DebugTemp(uint8_t*, uint8_t*, uint32_t, uint8_t*, uint32_t*, bool);
+char * _Print_Word(AES256_Word);
+char * _Print_State(AES256_State);
+AES256_Word SubWord(AES256_Word, bool);
+AES256_Word RotWord(AES256_Word, uint8_t);
+AES256_Word XorRcon(AES256_Word, uint8_t);
+AES256_Word XorWord(AES256_Word, AES256_Word);
+void KeyExpansion(uint8_t[], AES256_Word[]);
+AES256_State AddRoundKey(AES256_State, AES256_Word*, uint8_t);
+AES256_State SubBytes(AES256_State, bool);
+AES256_State ShiftRows(AES256_State);
+AES256_Word InvMixColumn(AES256_Word);
+AES256_Word MixColumn(AES256_Word);
+AES256_State MixColumns(AES256_State, bool);
+AES256_State InvShiftRows(AES256_State);
+
+#endif
---
+++ main.c
@@ -0,0 +1,115 @@
+#include <stdio.h>
+#include <unistd.h>
+#include "aes256.h"
+
+#define MAX_MSG_LGTH 1000000000 //1 GB
+
+void str_to_bytes(char *str, uint8_t *bytes){
+  int str_len = strlen(str);
+  int i;
+  for(i = 0; i < str_len; i++){
+    bytes[i] = str[i];
+  }
+}
+
+char* bytes_to_hexstr(uint8_t *bytes, uint32_t length){
+  static char buffer[(uint32_t)MAX_MSG_LGTH * 2];
+  memset(buffer, 0, strlen(buffer));
+  int i;
+  for(i = 0; i < length; i++){
+    sprintf(buffer + strlen(buffer), "%02x", bytes[i]);
+    if((i + 1) % 16 == 0){
+      sprintf(buffer + strlen(buffer),"\n");
+    }
+  }
+  return buffer;
+}
+
+char* bytes_to_str(uint8_t *bytes, uint32_t length){
+  static char buffer[(uint32_t)MAX_MSG_LGTH];
+  memset(buffer, 0, strlen(buffer));
+  int i;
+  for(i = 0; i < length; i++){
+    if(bytes[i] == 0 || 32 <= bytes[i] && bytes[i] <= 126)
+      sprintf(buffer + strlen(buffer), "%c", bytes[i]);
+    else
+      sprintf(buffer + strlen(buffer), "%c", '_');
+  }
+  return buffer;
+}
+
+void parse_02X(uint8_t *key, char *arg){
+  int iter;
+  for(iter = 0; iter < 32; iter ++){
+    uint8_t tempbit;
+    char temp[2] = "";
+    temp[0] = (char*)arg[iter*2];
+    temp[1] = (char*)arg[iter*2+1];
+    printf("%s\n", temp);
+    sscanf(temp, "%02hhX", &tempbit);
+    key[iter] = tempbit;
+  }
+}
+
+int main(int argc, char *argv[]){
+
+  uint8_t key[32] = {
+    0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,
+    0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f,
+    0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 
+    0x18, 0x19, 0x1a, 0x1b, 0x1c, 0x1d, 0x1e, 0x1f,
+  };
+
+  uint8_t proofdata[16] = {
+    0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77,
+    0x88, 0x99, 0xaa, 0xbb, 0xcc, 0xdd, 0xee, 0xff,
+  };
+
+  bool proof = false;
+
+  char * message = "";
+
+  if(argc == 1){
+    proof = true;
+  }
+  else if(argc == 2){
+    message = argv[1];
+  }else if(argc == 3){
+    message = argv[1];
+    char *keystring = argv[2];
+    if(strlen(keystring) != 64){
+      printf("key must be 32 bytes\n");
+      exit(-1);
+    }
+    parse_02X(key,keystring);
+  }else{
+    printf("Usage:\n%s [Message to encrypt]\n%s [Message to encrypt] [Key in 02x hex]\n",argv[0],argv[0]);
+    exit(-1);
+  }
+
+  uint8_t *byteBuffer = (uint8_t*)malloc((uint32_t)MAX_MSG_LGTH);
+  uint8_t *returnBuffer = (uint8_t*)malloc((uint32_t)MAX_MSG_LGTH);
+
+  if(byteBuffer == NULL || returnBuffer == NULL){
+    printf("error allocating buffers\n");
+    return(-1);
+  }
+
+  uint32_t returnlen = 0;
+  
+  printf("Key is...\n%s", bytes_to_hexstr(key, 32));
+  if(proof){
+    printf("Data is... \n%s", bytes_to_hexstr(&proofdata,16));
+    AES256Main(key, &proofdata, 16, returnBuffer, &returnlen, true);
+  }else{
+    str_to_bytes(message, byteBuffer);
+    printf("Message is... \n%s\n", message);
+    AES256Main(key, byteBuffer, strlen(message), returnBuffer, &returnlen, true);
+  }
+  printf("Encrypted data is...\n%s",bytes_to_hexstr(returnBuffer, returnlen));
+  uint8_t *newBuffer = (uint8_t*)malloc((uint32_t)MAX_MSG_LGTH);
+  AES256Main(key, returnBuffer, returnlen, newBuffer, &returnlen, false);
+  printf("Unencrypted data is...\n%s",bytes_to_hexstr(newBuffer, returnlen));
+  if(!proof)
+    printf("Unencrypted message is...\n%s\n",bytes_to_str(newBuffer, returnlen));
+}
